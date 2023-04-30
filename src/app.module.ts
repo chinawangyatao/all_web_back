7ff909e208ec693fd8d7c67272b3ea21eb6145b9
matapp <w1176317790@163.com>
2023-04-30 11:52:46 +0800

    修改本地文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,37 +12,38 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectModule } from './project/project.module';
 
-
-
 @Module({
-  imports: [BannerModule,TypeOrmModule.forRoot({
-    type: "mysql", //数据库类型
-    username: "root1", //账号
-    password: "88888888", //密码
-    host: "123.168.95.215", //host
-    port: 3306, //
-    database: "mybolg", //库名
+  imports: [
+    BannerModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', //数据库类型
+      username: 'root', //账号
+      password: '88888888', //密码
+      // host: '123.168.95.215',
+      host: '127.0.0.1', //host
+      port: 3306, //
+      database: 'mybolg', //库名
 
-    // 线上地址
-    // username: "nest_daidaidd", //账号
-    // password: "Fz7cptaC4ni4XC3M", //密码
-    // host: "127.0.0.1", //host
-    // port: 3306, //
-    // database: "nest_daidaidd", //库名
+      // 线上地址
+      // username: 'nest_daidaidd', //账号
+      // password: 'Fz7cptaC4ni4XC3M', //密码
+      // host: '127.0.0.1', //host
+      // port: 3306, //
+      // database: 'nest_daidaidd', //库名
 
-    // entities: [__dirname + '/**/*.entity{.ts,.js}'], //实体文件
-    synchronize:true, //synchronize字段代表是否自动将实体类同步到数据库
-    retryDelay:500, //重试连接数据库间隔
-    retryAttempts:10,//重试连接数据库的次数
-    autoLoadEntities:true, //如果为true,将自动加载实体 forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中
-  }),
-  AboutusModule,
-  NewarticleModule,
-  CodemoduleModule,
-  HouseModule,
-  RecommendModule,
-  UsersModule,
-  ProjectModule
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'], //实体文件
+      synchronize: true, //synchronize字段代表是否自动将实体类同步到数据库
+      retryDelay: 500, //重试连接数据库间隔
+      retryAttempts: 10, //重试连接数据库的次数
+      autoLoadEntities: true, //如果为true,将自动加载实体 forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中
+    }),
+    AboutusModule,
+    NewarticleModule,
+    CodemoduleModule,
+    HouseModule,
+    RecommendModule,
+    UsersModule,
+    ProjectModule,
   ],
   controllers: [AppController, CategoryController],
   providers: [AppService],
